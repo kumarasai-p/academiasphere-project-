f48d0f44e69ee9b65edc59e6adfbea372ce30d8e
kumarasai-p <kumarasaipabbisetty@gmail.com>
2025-09-19 15:42:27 +0530

    Update studentApi.js

--- frontend/src/services/studentApi.js
+++ frontend/src/services/studentApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://academiasphere-project.onrender.com';
+const API_URL = 'https://academiasphere-project.onrender.com/api/students';
 
 const getAll = () => {
     return axios.get(API_URL);
